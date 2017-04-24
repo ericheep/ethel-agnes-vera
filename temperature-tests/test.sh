1acd1add3d70f12c30c29fbc58af3526dd14dc63
ericheep <ericheep@gmail.com>
2017-04-24 04:45:22 +0000

    added benchmarking script for temp tests

---
+++ temperature-tests/test.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+clear
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+
+
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=2500 --num-threads=4 run >/dev/null 2>&1
+
+vcgencmd measure_temp
+
